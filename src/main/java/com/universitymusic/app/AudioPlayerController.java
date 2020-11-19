fb42bf22273278c8698b9def235c8893bc1228f1
ElC <mai1519@uom.edu.gr>
2020-11-19 23:19:39 +0200

    Artists and Album logic without UI format

--- src/main/java/com/universitymusic/app/AudioPlayerController.java
+++ src/main/java/com/universitymusic/app/AudioPlayerController.java
@@ -13,6 +13,7 @@ import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,14 +24,23 @@ public class AudioPlayerController {
 	@FXML
 	private Slider timeSlider = new Slider();
 	@FXML
-	private ListView<String> listView;
+	private ListView<String> listViewSongs;
+	@FXML
+	private ListView<String> listViewAlbums;
+	@FXML
+	private ListView<String> listViewArtists;
 	@FXML
 	private Button play;
 	@FXML
 	private Button pause;
 	@FXML
+	private Button albums;
+	@FXML
+	private Button songs;
+	@FXML
+	private Button artists;
+	@FXML
 	private ProgressBar progressBar = new ProgressBar();
-
 	@FXML
 	private Text currentDuration;
 	@FXML
@@ -40,27 +50,41 @@ public class AudioPlayerController {
 	private String currentSongPath;
 	private Duration totalTime;
 	private boolean firstTime = true;
+	private MusicDatabase db;
+	private List<Song> allsongs;
+	private List<Artist> allArtists;
 
-	ObservableList observableList = FXCollections.observableArrayList();
-	Map<String, String> titlePathMap = new HashMap();
-
-	public void setListView() {
-		MusicDatabase db = new MusicDatabase();
-		List<Song> allsongs = db.getAllSongs();
-
-		for (Song song : allsongs) {
-			observableList.add(song.title);
-			titlePathMap.put(song.title, song.path);
-		}
-		listView.getItems().addAll(observableList);
-	}
+	ObservableList observableListSongs = FXCollections.observableArrayList();
+	ObservableList observableListAlbums = FXCollections.observableArrayList();
+	ObservableList observableListArtists = FXCollections.observableArrayList();
+	Map<String, String> titlePathMap = new HashMap();;
+	Map<String, String> albumidAlbumtitleMap = new HashMap();
+	Map<String, String> artistidArtisttitleMap = new HashMap();
 
 	@FXML
 	void initialize() {
-		assert listView != null : "fx:id=\"listView\" was not injected: check your FXML file 'CustomList.fxml'.";
 
-		setListView();
+		db = new MusicDatabase();
 		pause.setDisable(true);
+		
+		//initialize all Songs
+		allsongs = db.getAllSongs();
+		
+		//initialize albums
+		List<Album> allAlbums = db.getAlbums();
+		
+		for (Album album: allAlbums) {
+			observableListAlbums.add(album.title);
+			albumidAlbumtitleMap.put(album.title,album.id);
+		}
+		
+		//initialize artists
+		allArtists = db.getArtists();
+		
+		for (Artist artist: allArtists) {
+			observableListArtists.add(artist.title);
+			artistidArtisttitleMap.put(artist.title,artist.id);
+		}
 	}
 
 	@FXML
@@ -73,7 +97,7 @@ public class AudioPlayerController {
 	@FXML
 	private void playAction(ActionEvent event) {
 		Media media;
-		String selectedSongTitle = listView.getSelectionModel().getSelectedItem();
+		String selectedSongTitle = listViewSongs.getSelectionModel().getSelectedItem();
 		String selectedSongPath = titlePathMap.get(selectedSongTitle);
 		if (firstTime) {
 			currentSongPath = selectedSongPath;
@@ -100,7 +124,89 @@ public class AudioPlayerController {
 			}
 		}
 	}
-
+	
+	@FXML
+	private void setListViewSongs() {
+		setSongs(allsongs,false);
+	}
+	
+	@FXML
+	private void setListViewAlbums() {
+		if (listViewAlbums != null) {
+			listViewAlbums.getItems().clear();
+			listViewAlbums.getItems().addAll(observableListAlbums);
+			
+			listViewAlbums.setVisible(true);
+			listViewSongs.setVisible(false);
+			
+			listViewAlbums.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			    @Override
+			    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			    	String albumId = albumidAlbumtitleMap.get(newValue);
+			    	setListViewAlbumSongs(albumId);
+			    }
+			});
+		} else {
+			System.out.println("There are no albums to preview!");
+		}
+	}
+	
+	@FXML
+	private void setListViewArtists() {		
+		if (listViewArtists != null) {
+			listViewArtists.getItems().clear();
+			listViewArtists.getItems().addAll(observableListArtists);
+			
+			listViewArtists.setVisible(true);
+			listViewAlbums.setVisible(false);
+			listViewSongs.setVisible(false);
+			
+			listViewArtists.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			    @Override
+			    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			    	String artistId = artistidArtisttitleMap.get(newValue);
+			    	setListViewArtistSongs(artistId);
+			    }
+			});
+		} else {
+			System.out.println("There are no Artists");
+		}
+		
+	}
+	
+	private void setSongs (List<Song> songs, boolean remainVisible) {
+		assert listViewSongs != null : "fx:id=\"listViewSongs\" was not injected: check your FXML file 'CustomList.fxml'.";
+		
+		observableListSongs.clear();
+		titlePathMap.clear(); 
+		for (Song song : songs) {
+			observableListSongs.add(song.title);
+			titlePathMap.put(song.title, song.path);
+		}
+		listViewSongs.getItems().clear();
+		listViewSongs.getItems().addAll(observableListSongs);
+		listViewSongs.setVisible(true);
+		if (listViewAlbums != null && !remainVisible) listViewAlbums.setVisible(false);
+		if (listViewArtists != null && !remainVisible) listViewArtists.setVisible(false);
+		
+	}
+	
+	private void setListViewAlbumSongs(String albumId) {
+		List<Song> albumSongs = db.getAlbumSongs(albumId);
+		setSongs(albumSongs,true);
+	}
+	
+	private void setListViewArtistSongs(String artistId) {
+		//εδώ το κάνουμρ από το array lisτ και όχι μέσω της db
+		List<Song> artistSongs = new ArrayList();
+		for (Song song: allsongs) {
+			if (song.artistId.equals(artistId)) {
+				artistSongs.add(song);
+			}
+		}
+		setSongs(artistSongs,true);
+	}
+	
 	private void applyDurationAndSlider(MediaPlayer player) {
 		player.currentTimeProperty().addListener(new ChangeListener<Duration>() {
 			@Override
